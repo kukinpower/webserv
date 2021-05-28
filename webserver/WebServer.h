6772506e590413452f650a570b96fe8e59024134
kukinpower <thekukinpower@gmail.com>
2021-05-28 21:14:07 +0300

    make it listening on port 9000

--- webserver/WebServer.h
+++ webserver/WebServer.h
@@ -23,6 +23,7 @@ class WebServer {
     return fd;
   }
 
+ private:
   void createSocket() {
     fd = socket(AF_INET, SOCK_STREAM, TCP);
     if (fd == -1) {
@@ -42,6 +43,14 @@ class WebServer {
     }
   }
 
+  void startListening() {
+    if (-1 == listen(fd, 5)) {
+      LOGGER.error(LISTEN_ERROR);
+      //todo throw custom exception
+    }
+  }
+
+ public:
   void run() {
     // todo add Parser and config
     // 1. parse args
@@ -49,6 +58,11 @@ class WebServer {
     createSocket();
     // 3. bind
     bindAddress();
+    // 4. listen
+    startListening();
+    while (true) {
+
+    }
 
   }
 
@@ -60,9 +74,11 @@ class WebServer {
   // messages temporarily here todo move to custom exceptions
   static const char *BAD_SOCKET_FD;
   static const char *BIND_ERROR;
+  static const char *LISTEN_ERROR;
 };
 
-const int WebServer::PORT_DEFAULT = 8080;
+const int WebServer::PORT_DEFAULT = 9000;
 
 const char *WebServer::BAD_SOCKET_FD = "Something went wrong";
 const char *WebServer::BIND_ERROR = "Bind error";
+const char *WebServer::LISTEN_ERROR = "Listen error";
