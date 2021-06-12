0f129d553fbcaca817205b028a6f9fc7c26cba3a
kukinpower <thekukinpower@gmail.com>
2021-06-12 16:58:46 +0300

    remove unused headers

--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
-#include "ConfigReader.h"
 #include "WebServer.h"
 #include "Logger.h"
 
 #include <iostream>
-#include <sys/types.h>
-#include <sys/socket.h>
 
 int main(int ac, char *av[]) {
   try {
